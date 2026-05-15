593e3645ef433810afddd92f3e979083b7b40ced
abdulmeliktuhaye <abdulmelikmohammednur123@gmail.com>
2026-05-14 22:07:51 -0500

    Hand class modified

--- src/main/java/com/pluralsight/Hand.java
+++ src/main/java/com/pluralsight/Hand.java
@@ -1,4 +1,45 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+
 public class Hand {
+    //Stores all cards in the hand
+    private ArrayList<Card> cards;
+
+    //Constructor
+    public Hand() {
+        //Create the ArrayList
+        cards = new ArrayList<>();
+    }
+    //Adds a card to the hand
+    public void deal (Card card){
+            cards.add(card);
+    }
+    //Returns the number of cards in the hand
+    public int getSize(){
+        return cards.size();
+    }
+    //Calculates the total blackjack value of the hand
+    public int getValue(){
+        int value = 0;
+
+        //Loop through all cards
+        for (Card card : cards){
+
+            //Add each card value
+            value += card.getPointValue();
+        }
+        return value;
+    }
+    //Returns a string version of the hand
+    @Override
+    public String toString(){
+        String output = "";
+
+        //Display every card
+        for (Card card : cards){
+            output += card + "\n";
+        }
+        return output;
+    }
 }
